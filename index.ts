271c57099592a59854b4310e5cbac68403404d37
SgtGarro <marcelogarro137@gmail.com>
2024-03-10 12:12:09 -0500

    Init REST API

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-console.log('Hello world')
-const name = 'Marcelo'
-console.log(name)
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world from my!')
+})
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello world from new route!')
+})
+
+app.get('/products', (req, res) => {
+  res.json({ name: 'Marcelo' })
+})
+
+app.listen(port, () => console.log('Server started on port', port))
